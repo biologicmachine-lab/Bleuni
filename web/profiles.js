a5f43deaa1dbb35a6fc3be5f1a5bfe7f23444344
biologicmachine-lab <bio.logic.machine@gmail.com>
2026-02-09 03:03:09 -0500

    Add profiles for UART, Heart Rate, and Battery

---
+++ web/profiles.js
@@ -0,0 +1,15 @@
+export const PROFILES = {
+  NONE: {},
+  UART: {
+    service: "6e400001-b5a3-f393-e0a9-e50e24dcca9e",
+    characteristic: "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
+  },
+  HEART_RATE: {
+    service: "180d",
+    characteristic: "2a37"
+  },
+  BATTERY: {
+    service: "180f",
+    characteristic: "2a19"
+  }
+};
